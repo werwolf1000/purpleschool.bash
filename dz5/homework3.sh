7b7fb5140b68a5dab632781407e2c765c62a3c59
werwolf <werwolf@webmastermsk.ru>
2025-06-15 03:06:46 +0300

    fix

--- dz5/homework3.sh
+++ dz5/homework3.sh
@@ -1,31 +1,29 @@
 #!/bin/bash
 
-
-if [ $# -eq 0 ];
-then
-    echo "Не переданы входные параметры"
-    exit 1
-fi
-
-num=$1
-
-func() 
-{   
-        if [ $num -gt 0 ];            
-        then        
-            result="Простое число"
-            for (( i=1; i < num; i++ ))
-            do
-                if [ $(( num % i )) -eq 0  ] && [ $num -ne $i ]  && [ $i -gt 1 ];                
-                then
-                    result="Не простое число"
-                    break
-                fi    
-            done
-        else
-            result="Не простое число"
+# Функция для проверки, является ли число простым
+is_prime() {
+    local num=$1
+    
+    # Обработка случаев для чисел меньше 2
+    if [ $num -lt 2 ]; then
+        return 1  # 1 - не простое (кроме 2, но 0 и 1 - нет)
+    fi
+    
+    # Проверка делителей от 2 до sqrt(num)
+    for ((i=2; i*i<=num; i++)); do
+        if [ $((num % i)) -eq 0 ]; then
+            return 1  # Найден делитель - число не простое
         fi
-        echo $result
+    done
+    
+    return 0  # Число простое
 }
 
-func
+# Основная часть скрипта
+read -p "Введите число: " number
+
+if is_prime "$number"; then
+    echo "$number — простое число."
+else
+    echo "$number — не является простым числом."
+fi
